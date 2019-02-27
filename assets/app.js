477d402cd7ae455f9507840ac21abb20d0c57d1a
Perry Sham <perrysham@Perrys-MBP.home>
2019-02-27 11:23:56 -0500

    2/27

--- assets/app.js
+++ assets/app.js
@@ -7,113 +7,85 @@ $(document).ready(function() {
 
     
     // list questions
-    var triviaQuestions = {
+    var trivia = [{
         q1: "What is the largest bone in the human body?",
+        o1: ["Humerus", "Femur", "Vertebrae", "Radius"],
+        a1: "Humerus"
+    },{
         q2: "What is removed during a cholecystectomy?",
+        o2: ["Pancreas", "Appendix", "Colon", "Gallbladder"],
+        a2: "Gallbladder"
+    },{
         q3: "Where is your buccal cavity?",
+        o3: ["Mouth", "Colon", "Ear", "Nose"],
+        a3: "Mouth"
+    },{
         q4: "What does a sphygmomanometer measure?",
+        o4: ["Blood pressure", "Cholesterol", "Blood glucose levels", "Sodium levels"],
+        a4: "Blood pressure"
+    },{
         q5: "What is varicella better known as?",
+        o5: ["The flu", "Measles", "Chicken Pox", "Polio"],
+        a5: "Chicken Pox"
+    },{
         q6: "What part of the body does atherosclerosis affect?",
+        o6: ["Heart", "Lungs", "Veins", "Arteries"],
+        a6: "Arteries"
+    },{
         q7: "Creutzfeldt-Jakob disease is the human variant of what disease?",
+        o7: ["Swine flu", "Mad cow disease", "Avian flu", "Monkeypox"],
+        a7: "Mad cow disease"
+    },{
         q8: "What is the top leading cause of death in the US?",
+        o8: ["Heart disease", "Cancer", "Car accidents", "Stroke"],
+        a8: "Heart disease"
+    },{
         q9: "What does the field of bariatrics focus on?",
-        q10: "What part of the body is used during mastication?"
-    }
+        o9: ["Barium", "Sleep", "Lung pressure", "Obesity"],
+        a9: "Obesity"
+    },{
+        q10: "What part of the body is used during mastication?",
+        o10: ["Genitals", "Jaw", "Large intestine", "Small intestine"],
+        a10: "Jaw"
+    }];
 
-    var triviaOptions = {
-        q1: {
-            a: "Humerus",
-            b: "Femur",
-            c: "Vertebrae",
-            d: "Radius"
-        },
-        q2: {
-            a: "Pancreas",
-            b: "Appendix",
-            c: "Colon",
-            d: "Gallbladder"
-        },
-        q3: {
-            a: "Mouth",
-            b: "Colon",
-            c: "Ear",
-            d: "Nose"
-        },
-        q4: {
-            a: "Blood Pressure",
-            b: "Cholesterol",
-            c: "Blood glucose levels",
-            d: "Sodium levels"
-        },
-        q5: {
-            a: "The flu",
-            b: "Measles",
-            c: "Chicken Pox",
-            d: "Polio"
-        },
-        q6: {
-            a: "Heart",
-            b: "Lungs",
-            c: "Veins",
-            d: "Arteries"
-        },
-        q7: {
-            a: "Swine flu",
-            b: "Mad cow disease",
-            c: "Avian flu",
-            d: "Monkeypox"
-        },
-        q8: {
-            a: "Heart disease",
-            b: "Cancer",
-            c: "Car accidents",
-            d: "Stroke"
-        },
-        q9: {
-            a: "Barium",
-            b: "Sleep",
-            c: "Lung pressure",
-            d: "Obesity"
-        },
-        q10: {
-            a: "Genitals",
-            b: "Mouth",
-            c: "Large intestine",
-            d: "Small intestine"
+    // initially hides option box
+    $("#optionBox").hide();
+
+    // display and ask questions
+    $("#btn1").click(function(){
+        $("#btn1").hide();
+        $("#question").append(trivia[0]["q1"]);
+        $("#optionBox").show();
+        $("#option1").append(trivia[0]["o1"][0]);
+        $("#option2").append(trivia[0]["o1"][1]);
+        $("#option3").append(trivia[0]["o1"][2]);
+        $("#option4").append(trivia[0]["o1"][3]);
+        startTimer;
+    });
+
+    var userGuess = $("#option1").val();
+    console.log(userGuess);
+
+    // set timer function
+    function countDown(){
+        $("#timer").text("Time: " + timer);
+        if(timer === 0){
+            timer.stopTimer();
+            $("#timer").empty();
         }
     }
 
-    var triviaAnswers = {
-        q1: "a",
-        q2: "d",
-        q3: "a",
-        q4: "a",
-        q5: "c",
-        q6: "d",
-        q7: "b",
-        q8: "a",
-        q9: "d",
-        q10: "b"
+    function startTimer(){
+        $("#timer").text("Time: " + timer);
+        setInterval(countDown, 10000);
     }
 
-    // display and loop through questions
-    $("#btn1").click(function(){
-        $("#btn1").hide();
-    });
-    console.log("#btn1");
-
-    function nextQuestion() {
-        
-
+    function stopTimer(){
+        clearInterval();
     }
-
-    // set timer
-    $("#timer").text(triviaAnswers.timer);
-
-
-    // correct or incorrect answer
     
-
+    // correct or incorrect answer
 
     // reset
     function startGame() {
@@ -124,7 +96,6 @@ $(document).ready(function() {
         clearInterval(triviaQuestions.timer);
     
     }
-    console.log(startGame);
     
 
 });
